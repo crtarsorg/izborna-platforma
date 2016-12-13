6a590f05e12e242814f8fcef2f0d4c00a5b408f1
ferideadili <feride_adili@hotmail.com>
2016-12-13 21:03:03 +0100

    territories and political parties results

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -14,32 +14,32 @@ mod_main = Blueprint('main', __name__)
 def index():
     #year 2014 parlamentary
     data2014 = []
-    url2014 = "http://datacentar.io/api/izbori/2/parlamentarni/godina/2014/instanca/4"
+    url2014 = "http://0.0.0.0:5006/api/izbori/2/parlamentarni/godina/2014/instanca/4"
     response2014 = urllib.urlopen(url2014)
     for line2014 in json.loads(response2014.read()):
         data2014.append(line2014)
     #end of year 2014 parlamentary
 
     data2016 = []
-    url2016 = "http://datacentar.io/api/izbori/2/parlamentarni/godina/2016/instanca/4"
+    url2016 = "http://0.0.0.0:5006/api/izbori/2/parlamentarni/godina/2016/instanca/4"
     response2016 = urllib.urlopen(url2016)
     for line2016 in json.loads(response2016.read()):
         data2016.append(line2016)
     #end of year 2016 presidential
 
     data2008pres=[]
-    url2008pres = "http://datacentar.io/api/izbori/1/predsjednicki/godina/2008/instanca/1"
+    url2008pres = "http://0.0.0.0:5006/api/izbori/1/predsjednicki/godina/2008/instanca/1"
     response2008pres = urllib.urlopen(url2008pres)
     for line2008pres in json.loads(response2008pres.read()):
         data2008pres.append(line2008pres)
 
     data2012pres = []
-    url2012pres = "http://datacentar.io/api/izbori/1/predsjednicki/godina/2012/instanca/1"
+    url2012pres = "http://0.0.0.0:5006/api/izbori/1/predsjednicki/godina/2012/instanca/1"
     response2012pres = urllib.urlopen(url2012pres)
     for line2012pres in json.loads(response2012pres.read()):
         data2012pres.append(line2012pres)
 
-    url_political_parties = "http://datacentar.io/api/izbori/parties"
+    url_political_parties = "http://0.0.0.0:5006/api/izbori/parties"
     response_political_parties = urllib.urlopen(url_political_parties)
     list_political_parties = json_util.loads(response_political_parties.read())
 
--- app/mod_type/views.py
+++ app/mod_type/views.py
@@ -12,35 +12,51 @@ mod_type = Blueprint('type', __name__, url_prefix="/type",static_folder="static"
 @mod_type.route('/<int:datasource>/<string:typeizbori>/<int:year>/<int:instanca>', methods=['GET'])
 def electionType(datasource, typeizbori,year, instanca):
     data= []
-    url= "http://datacentar.io/api/izbori/2/"+str(typeizbori)+"/godina/"+str(year)+"/instanca/"+str(instanca)
+    if year==2000:
+        url= "http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/instanca/3"
+    elif year==2007:
+        url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/1"
+    else:
+        url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/3"
+
     response= urllib.urlopen(url)
     for line in json.loads(response.read()):
         data.append(line)
     #get all territories by selected yea
     territories=[]
-    urlterritories="http://datacentar.io/api/izbori/2/"+str(typeizbori)+"/godina/"+str(year)+"/teritorija/instanca/3"
+    if year == 2000:
+        urlterritories="http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/teritorija/instanca/3"
+    else:
+        urlterritories = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/teritorija/instanca/3"
     responseterritories = urllib.urlopen(urlterritories)
     for territory in json.loads(responseterritories.read()):
         territories.append(territory)
 
     #total voters turnout
-    total_percentage=0
-    urlturnout="http://datacentar.io/api/izbori/parlamentarni/godina/"+str(year)
+
+    urlturnout = "http://0.0.0.0:5006/api/izbori/datasource/2/parlamentarni/godina/" + str(year)
     response_turnout = urllib.urlopen(urlturnout)
-    total_percentage=response_turnout.read();
+    results_voters=json.loads(response_turnout.read())
+
+
 
     #get all political parties
-    url_political_parties = "http://datacentar.io/api/izbori/parties"
+    url_political_parties = "http://0.0.0.0:5006/api/izbori/parties"
     response_political_parties = urllib.urlopen(url_political_parties)
     list_political_parties = json_util.loads(response_political_parties.read())
 
 
     #get winners for each territory
     winner_territory = []
-    url_winner_territory = "http://datacentar.io/api/izbori/api/izbori/winners/2/"+str(typeizbori)+"/godina/"+str(year)+"/instanca/3"
+    if year==2000:
+        url_winner_territory = "http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/teritorija/instanca/3"
+    elif year==2007:
+        url_winner_territory = "http://0.0.0.0:5006/api/izbori/1/" + str(typeizbori) + "/godina/" + str( year) + "/teritorija/instanca/1"
+    else:
+        url_winner_territory = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/teritorija/instanca/3"
     response_winner = urllib.urlopen(url_winner_territory)
     for winner in json_util.loads(response_winner.read()):
         winner_territory.append(winner)
-    return render_template('type/type.html',typeizbori=typeizbori,year=year,data=data,territories=territories,total_percentage=total_percentage,list_political_parties=list_political_parties,winner_territory=winner_territory)
+    return render_template('type/type.html',typeizbori=typeizbori,year=year,data=data,territories=territories,results_voters=results_voters,list_political_parties=list_political_parties,winner_territory=winner_territory)
 
 
