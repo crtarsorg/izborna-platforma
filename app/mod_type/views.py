c31c4a129be3348a77f6fe0ddd61fb71c7456ec0
ferideadili <feride_adili@hotmail.com>
2016-12-14 20:25:56 +0100

    presindtial

--- app/mod_type/views.py
+++ app/mod_type/views.py
@@ -9,36 +9,50 @@ from bson import json_util
 mod_type = Blueprint('type', __name__, url_prefix="/type",static_folder="static", template_folder='templates', )
 
 #get data for all territories selected by year
-@mod_type.route('/<int:datasource>/<string:typeizbori>/<int:year>/<int:instanca>', methods=['GET'])
-def electionType(datasource, typeizbori,year, instanca):
+@mod_type.route('/<int:datasource>/<string:typeizbori>/<int:year>/<int:instanca>/krug/<string:krug>', methods=['GET'])
+def electionType(datasource, typeizbori,year, instanca, krug):
     data= []
-    if year==2000:
-        url= "http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/instanca/3"
-    elif year==2007:
-        url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/1"
+    if typeizbori!="predsjednicki":
+        if year==2000:
+            url= "http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/instanca/3/krug/none"
+        elif year==2007 or year==2016:
+            url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/4/krug/none"
+        else:
+            url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/3/krug/none"
     else:
-        url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/3"
+        if year==2004:
+            url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/4/krug/"+str(krug)
+        else:
+            url = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/3/krug/" + str(krug)
 
     response= urllib.urlopen(url)
     for line in json.loads(response.read()):
         data.append(line)
-    #get all territories by selected yea
+    #get all territories by selected year
     territories=[]
     if year == 2000:
         urlterritories="http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/teritorija/instanca/3"
+    elif year == 2007 or year==2016:
+        urlterritories = "http://0.0.0.0:5006/api/izbori/2/parlamentarni/godina/2007/teritorija/instanca/4"
+    elif year==2004 and typeizbori=="predsjednicki":
+        urlterritories = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/teritorija/instanca/4"
     else:
-        urlterritories = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/teritorija/instanca/3"
-    responseterritories = urllib.urlopen(urlterritories)
-    for territory in json.loads(responseterritories.read()):
-        territories.append(territory)
+        urlterritories = "http://0.0.0.0:5006/api/izbori/winners/2/" + str(typeizbori) + "/godina/" + str(year) + "/instanca/3"
+    if year!=2007:
+        responseterritories = urllib.urlopen(urlterritories)
+        for territory in json.loads(responseterritories.read()):
+            territories.append(territory)
+    else:
+        responseterritories = urllib.urlopen(urlterritories)
+        for territory in json.loads(responseterritories.read()):
+            territories.append(territory)
+
 
     #total voters turnout
 
-    urlturnout = "http://0.0.0.0:5006/api/izbori/datasource/2/parlamentarni/godina/" + str(year)
+    urlturnout = "http://0.0.0.0:5006/api/izbori/datasource/2/" + str(typeizbori) + "/godina/" + str(year)
     response_turnout = urllib.urlopen(urlturnout)
     results_voters=json.loads(response_turnout.read())
-
-
 
     #get all political parties
     url_political_parties = "http://0.0.0.0:5006/api/izbori/parties"
@@ -50,13 +64,15 @@ def electionType(datasource, typeizbori,year, instanca):
     winner_territory = []
     if year==2000:
         url_winner_territory = "http://0.0.0.0:5006/api/izbori/1/"+str(typeizbori)+"/godina/"+str(year)+"/teritorija/instanca/3"
-    elif year==2007:
-        url_winner_territory = "http://0.0.0.0:5006/api/izbori/1/" + str(typeizbori) + "/godina/" + str( year) + "/teritorija/instanca/1"
+    elif year==2007 or year==2016:
+        url_winner_territory = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str( year) + "/teritorija/instanca/4"
     else:
-        url_winner_territory = "http://0.0.0.0:5006/api/izbori/2/" + str(typeizbori) + "/godina/" + str(year) + "/teritorija/instanca/3"
+        url_winner_territory = "http://0.0.0.0:5006/api/izbori/winners/2/" + str(typeizbori) + "/godina/" + str( year) + "/instanca/3"
     response_winner = urllib.urlopen(url_winner_territory)
+
     for winner in json_util.loads(response_winner.read()):
         winner_territory.append(winner)
+    print winner_territory
     return render_template('type/type.html',typeizbori=typeizbori,year=year,data=data,territories=territories,results_voters=results_voters,list_political_parties=list_political_parties,winner_territory=winner_territory)
 
 
